694384dc8f590d92b038283f2beadaf5662f16b1
alevinicius <alevinicius106@gmail.com>
2021-03-08 20:24:53 -0300

    Validações no Modelo -Feitas as validações dos campos no PilotoModelo através dos DataAnnotations. As DataAnnotations são colocadas antes da propriedade do Modelo, as ulitizadas em questão foram Required, MinLength e MaxLegnth nos campos Nome e Sobrenome

--- RallyDakar.API/Modelo/PilotoModelo.cs
+++ RallyDakar.API/Modelo/PilotoModelo.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RallyDakar.API.Modelo
 {
     public class PilotoModelo
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é de preenchimento obrigatório")]
+        [MinLength(5, ErrorMessage = "Nome deve ter no mínimo 5 caracteres")]
+        [MaxLength(50, ErrorMessage = "Nome não pode ter mais do que 50 caracteres")]
         public string Nome { get; set; }
-
+        [Required(ErrorMessage = "Sobrenome é de preenchimento obrigatório")]
+        [MinLength(5, ErrorMessage = "Sobrenome deve ter no mínimo 5 caracteres")]
+        [MaxLength(50, ErrorMessage = "Sobrenome não pode ter mais do que 50 caracteres")]
         public string SobreNome { get; set; }
         public int EquipeId { get; set; }
 
